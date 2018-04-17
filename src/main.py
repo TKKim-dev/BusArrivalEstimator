7001dddc9161aa29cfe27866eacbbbd432a6c6d1
alphavoc <31835492+alphavoc@users.noreply.github.com>
2018-04-17 09:37:21 +0900

    Delete exit codes

--- src/main.py
+++ src/main.py
@@ -4,11 +4,9 @@ from requester import *
 from parse import *
 from compare import *
 
-current, is_seoul_bus, route_name, routeID = {}, False, '88A', 218000018
+current, is_seoul_bus, route_name, routeID = {}, True, '5513', 100100251
 def repeat(second = 1.0):
     global current, is_seoul_bus, route_name, routeID
-    if datetime.datetime.now().hour == 3: # 새벽 3시가 되면 쓰레드를 종료
-        return
     response = parse(is_seoul_bus, get_response(is_seoul_bus, routeID))
     if current == {}:
         compare(route_name, response, current)
@@ -16,7 +14,7 @@ def repeat(second = 1.0):
     else:
         compare(route_name, response, current)
         current = response
-    print('Repeating...')
+    #print('Repeating...')
     threading.Timer(second, repeat, [second]).start()
     
 repeat(3.0)
