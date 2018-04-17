720d89ce712dcaa272fdc6be75f5c2ef2b3d3656
alphavoc <31835492+alphavoc@users.noreply.github.com>
2018-04-17 09:29:57 +0900

    Fix codes with exception handling
    Exception on Timeout

--- src/requester.py
+++ src/requester.py
@@ -1,5 +1,5 @@
 # @requester.py
-import requests
+import requests, time
 
 def get_response(is_seoul_bus, routeId):
     if is_seoul_bus:
@@ -13,5 +13,10 @@ def get_response(is_seoul_bus, routeId):
             'serviceKey': '1234567890',
             'routeId': routeId
         }
-        response = requests.get("http://openapi.gbis.go.kr/ws/rest/buslocationservice", params=params)
+		try:
+			response = requests.get("http://openapi.gbis.go.kr/ws/rest/buslocationservice", params=params)
+		except:
+			print(time.strftime("%Y-%m-%d %H:%M:%S]", "Connection Time Out. Sleeping for 30 minutes"))
+			time.sleep(1800)
+			return ''
         return response.text
